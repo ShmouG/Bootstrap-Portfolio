8bb654de30794f6d3cb079e85d41d34a3ceb0bfe
Mobious <sammiel.gross@gmail.com>
2018-08-12 17:32:40 -0500

    ababazaba

--- java.js
+++ java.js
@@ -1,21 +1,21 @@
 $(document).ready(() => {
-    var beegee = document.getElementById("beegee");
-    var catata = document.getElementById("catata");
+    // var beegee = document.getElementById("beegee");
+    // var catata = document.getElementById("catata");
 
-    $('.text2').on('mouseenter', () => {
-        beegee.play()
-     
-    })
-    $('.text2').on('mouseleave', () => {
-        beegee.pause()
-    });
-    $('.text3').on('mouseenter', () => {
-        catata.play()
-    })
-    $('.text3').on('mouseleave', () => {
-        catata.pause()
+    // $('.text2').on('click', () => {
+    //     beegee.play()
 
-    });
+    // })
+    // $('.text2').on('mouseleave', () => {
+    //     beegee.pause()
+    // });
+    // $('.text3').on('click', () => {
+    //     catata.play()
+    // })
+    // $('.text3').on('mouseleave', () => {
+    //     catata.pause()
+
+    // });
     // $("#imgAnimate").hover(
     //     function()
     //     {
